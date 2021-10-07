71e74969d8a97016b4a998917648f90557a324c8
薛良伟 <xueliangwei@Haha-2.local>
2021-10-07 16:20:36 -0400

    Added lifecycle methods on CreateNewTask activity

--- app/src/main/java/com/example/quiettime/CreateNewTask.java
+++ app/src/main/java/com/example/quiettime/CreateNewTask.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class CreateNewTask extends AppCompatActivity {
+    public static final String CAT = "CreateNewTask";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,43 @@ public class CreateNewTask extends AppCompatActivity {
 
         Intent intent = getIntent();
         //TextView textView = findViewById(R.id.textView);
+        Log.i(CAT,"onCreate");
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Log.i(CAT, "onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Log.i(CAT, "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Log.i(CAT, "onPause");
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Log.i(CAT, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Log.i(CAT, "onDestroy");
+    }
+
 }
