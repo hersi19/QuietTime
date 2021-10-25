c9cadfe6611e46096a56d64acfa4493315f51af9
DavidXue777 <xue.409@osu.edu>
2021-10-25 12:14:33 -0400

    Registration to database

--- app/src/main/java/com/example/quiettime/LogInActivity.java
+++ app/src/main/java/com/example/quiettime/LogInActivity.java
@@ -5,21 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
 
-
-
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FirebaseAuth mAuth;
         mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
     @Override
     protected void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+    public void reload(){
+
+    }
+    public void updateUI(FirebaseUser user){
 
     }
 
