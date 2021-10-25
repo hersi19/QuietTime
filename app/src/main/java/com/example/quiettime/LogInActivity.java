752eb16c625e6aaba4fd76f26a21efa65aaf7bbb
DavidXue777 <xue.409@osu.edu>
2021-10-25 11:23:17 -0400

    Registration to database

--- app/src/main/java/com/example/quiettime/LogInActivity.java
+++ app/src/main/java/com/example/quiettime/LogInActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LogInActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseAuth mAuth;
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
     }
+    @Override
+    protected void onStart(){
+
+    }
+
 }
--- app/src/main/java/com/example/quiettime/RegisterAccountActivity.java
+++ app/src/main/java/com/example/quiettime/RegisterAccountActivity.java
@@ -3,14 +3,53 @@ package com.example.quiettime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class RegisterAccountActivity extends AppCompatActivity {
     public static final String REG = "RegisterAccountActivity";
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    //input variables
+    EditText regUserName,regEmail,reg_phoneNumber,regPassword;
+    Button create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+
+        //assign each input
+        regUserName = findViewById(R.id.reg_Username);
+        regEmail = findViewById(R.id.reg_Email);
+        reg_phoneNumber = findViewById(R.id.reg_Phone);
+        regPassword = findViewById(R.id.reg_Password);
+        create = findViewById(R.id.RegisterAccountActivity_Create);
+
+        //handle Create button
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference("users");
+                String userName = regUserName.getText().toString();
+                String email = regEmail.getText().toString();
+                String phone = reg_phoneNumber.getText().toString();
+                String password = regPassword.getText().toString();
+                AccountHelper one = new AccountHelper(userName,email,phone,password);
+                myRef.child(phone).setValue(one);
+            }
+        });
     }
 
 
